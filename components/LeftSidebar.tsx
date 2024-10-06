54e85606731e547df01a7425f80327026b001ad0
Georgi Stankov <vangog06406@gmail.com>
2024-10-06 17:13:26 +0300

    Skip re-rendering nav links when its props are unchanged.

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -3,9 +3,38 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { memo } from "react";
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+
+type SidebarLinktype = {
+  label: string;
+  route: string;
+  imgURL: string;
+  isActive: boolean;
+};
+
+const SidebarLink = memo(
+  ({ label, route, imgURL, isActive }: SidebarLinktype) => {
+    console.log("here...");
+    return (
+      <Link
+        key={label}
+        href={route}
+        className={cn(
+          "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+          {
+            "bg-nav-focus border-r-4 border-orange-1": isActive,
+          }
+        )}
+      >
+        <Image src={imgURL} alt={label} width={24} height={24} />
+        <p>{label}</p>
+      </Link>
+    );
+  }
+);
 
 const LeftSidebar = () => {
   const pathname = usePathname();
@@ -29,19 +58,13 @@ const LeftSidebar = () => {
             pathname === route || pathname.startsWith(`${route}/`);
 
           return (
-            <Link
+            <SidebarLink
               key={label}
-              href={route}
-              className={cn(
-                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
-                {
-                  "bg-nav-focus border-r-4 border-orange-1": isActive,
-                }
-              )}
-            >
-              <Image src={imgURL} alt={label} width={24} height={24} />
-              <p>{label}</p>
-            </Link>
+              label={label}
+              route={route}
+              imgURL={imgURL}
+              isActive={isActive}
+            />
           );
         })}
       </nav>
